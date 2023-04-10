90bbf1bd9f5441922d24e5a9ca8707bb3ebd0159
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-04-10 01:33:00 -0300

    Locations✅

--- src/components/LocationCard/LocationCard.jsx
+++ src/components/LocationCard/LocationCard.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 const Card = styled.div`
+  background-color: black;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   &:hover {
