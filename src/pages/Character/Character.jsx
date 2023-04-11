be7f318cd9be5df2efd4a44ce6bf0373a7b8e882
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-04-11 01:12:52 -0300

    ✅

--- src/pages/Character/Character.jsx
+++ src/pages/Character/Character.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const CharacterDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Character = () => {
-  return (
-    <div>Character</div>
-  )
-}
+  const { id } = useParams(); // retrieve character id from URL parameter
+  const [character, setCharacter] = useState(null); // state to hold character data
 
-export default Character
+  // fetch character data from API using character id
+  useEffect(() => {
+    const fetchCharacter = async () => {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/${id}`
+      );
+      const data = await response.json();
+      setCharacter(data);
+    };
+    fetchCharacter();
+  }, [id]);
+
+  // render character information when data is loaded
+  return (
+    <CharacterDetailContainer>
+      {character ? (
+        <>
+          <img src={character.image} alt={character.name} />
+          <h1>{character.name}</h1>
+          <p>Status: {character.status}</p>
+          <p>Species: {character.species}</p>
+          <p>Gender: {character.gender}</p>
+          <p>Origin: {character.origin.name}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </CharacterDetailContainer>
+  );
+};
+
+export default Character;
